ab14fdc615da9b71a118fc0ec3c5675ce0d5ff82
wangyejun <463512641@qq.com>
2017-05-03 08:28:17 +0800

    Fragment/Activity

--- app/src/main/java/com/example/niit/greenrouse/Activity/LoginActivity.java
+++ app/src/main/java/com/example/niit/greenrouse/Activity/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.example.niit.greenrouse.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.niit.greenrouse.MainActivity;
 import com.example.niit.greenrouse.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -21,12 +24,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         //控件初始化
         initView();
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    private void  initView(){
-        LoginName = (EditText)findViewById(R.id.et_LoginName);
-        LoginPwd = (EditText)findViewById(R.id.et_LoginPwd);
-        Login = (Button)findViewById(R.id.btn_Regist);
-        Regist = (TextView)findViewById(R.id.tv_regist);
-     }
+    private void initView() {
+        LoginName = (EditText) findViewById(R.id.et_LoginName);
+        LoginPwd = (EditText) findViewById(R.id.et_LoginPwd);
+        Login = (Button) findViewById(R.id.btn_Login);
+        Regist = (TextView) findViewById(R.id.tv_regist);
+    }
+
 }
--- app/src/main/java/com/example/niit/greenrouse/MainActivity.java
+++ app/src/main/java/com/example/niit/greenrouse/MainActivity.java
@@ -2,12 +2,58 @@ package com.example.niit.greenrouse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.niit.greenrouse.Fragment.BenefitFragment;
+import com.example.niit.greenrouse.Fragment.HomeFragment;
+import com.example.niit.greenrouse.Fragment.KnapsackFragment;
+import com.example.niit.greenrouse.Fragment.TradeFragment;
+import com.example.niit.greenrouse.Fragment.WalkFragment;
+
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+    private RadioGroup radioGroup;
+
+    HomeFragment homeFragment = new HomeFragment();
+    BenefitFragment benefitFragment = new BenefitFragment();
+    TradeFragment tradeframent = new TradeFragment();
+    KnapsackFragment knapsackfranment = new KnapsackFragment();
+    WalkFragment walkFragment = new WalkFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InitView();
+        InitEvent();
+    }
+    private void InitView(){
+        radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
+        radioGroup.check(R.id.rb_home);
+        getSupportFragmentManager().beginTransaction().replace(R.id.framlayout,homeFragment).commit();
+    }
+    public void InitEvent(){
+        radioGroup.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int i) {
+        switch (i){
+            case R.id.rb_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framlayout,homeFragment).commit();
+                break;
+            case R.id.rb_benefit:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framlayout,benefitFragment).commit();
+                break;
+            case R.id.rb_trade:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framlayout,tradeframent).commit();
+                break;
+            case R.id.rb_walk:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framlayout,walkFragment).commit();
+                break;
+            case R.id.rb_knapsack:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framlayout,knapsackfranment).commit();
+        }
+
     }
 }
